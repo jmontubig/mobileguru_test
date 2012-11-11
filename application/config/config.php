974864d33359a5611a3cbfc738690f86396dd75f
Jonathan Montubig <ayamereyes@yahoo.com>
2012-11-10 22:41:55 -0800

    Added homepage.php added css addedd js added custom fonts

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '8421164823ebe929444a64390fbd2e74aa7c5852';
 
 /*
 |--------------------------------------------------------------------------
